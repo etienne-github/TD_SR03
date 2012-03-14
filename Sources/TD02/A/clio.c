906976084dd4cb02a56b2458d905b96c9802592b
Etienne Girot <etienne.girot@hotmail.fr>
2012-03-14 16:50:39 +0100

    Après TD en classe

--- Sources/TD02/A/clio.c
+++ Sources/TD02/A/clio.c
@@ -1,42 +1,111 @@
-#include "defobj.h"
-#include "iniobj.h"
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 #include <string.h>
 
 
+#include "defobj.h"
+
+
+#include <sys/socket.h>
+#include "iniobj.h"
+
+
+#include <stdio.h>
+
+
+#include <netdb.h> //pour gethostbyname
 
 
 
 
 
-int main(void){
+
+int main(int argc, char* argv[]){
 
 	obj * tableObj = initTable();
-	
+
 	if (!tableObj){
-		
+
 		fprintf(stderr,"Client - Erreur dans l'initialisation de la table d'objet.\n");
 		exit(-1);
-	
+
+
+
 	}
+
+
+	int ClntSocket = socket(PF_INET,SOCK_STREAM,IPPROTO_TCP);
+	int ServSocket;
+	struct sockaddr_in echoServAddr; /*Adresse locale*/ //NB in = internet pas in/out
+	struct sockaddr_in echoClntAddr; /*Adresse client*/
+	struct hostent * hid;
+
+	//PF_INET = domaine de la socket = internet
+	//SOCK_STREAM = Mode connecté = TCP
 	
-	/*      
+
+	//Traitement des erreur
+	if (ClntSocket < 0){
+
+		perror("Serveur : Erreur pendant la creation de la Socket");
+		exit(-1);
+	}
+
+
+	//SOCKET DU CLIENT
+
+	//function bzero, plus rapide ?
+	memset(&echoClntAddr,0,sizeof(echoClntAddr)); /*mise à zero de la structure*/
+
+	echoClntAddr.sin_family = AF_INET; /*internet address family*/ //= domaine d'écoute
+	echoClntAddr.sin_addr.s_addr = htonl(INADDR_ANY); /*n'importe quelle interface entrante*/ // = Sur quelle carte réseau j'écoute ? //hton1 = conversion little endian big endian h(host) to nl ( network long) 
+	echoClntAddr.sin_port = 0; /*Port local, 0 laisse le choix à l'OS*/ 
+
+//LIASON DE LA SOCKET CLIENT
+
+	if(bind(ClntSocket,(struct sockaddr *) &echoClntAddr,sizeof(echoClntAddr)) < 0)  // ou alors juste faire un scocket() et un connect(ClntSocket,&echoClntAddr,0,sizeof(echoClntAddr)) qui va initialiser la structure adresse echoClntAddr tout seul 
+	{
+
+		perror("Client - Erreur lors de la connexion de la socket");
+		exit(-1);
+	}
+
+//SOCKET DU SERVEUR
+	memset(&echoServAddr,0,sizeof(echoServAddr)); /*mise à zero de la structure*/
+
+	echoServAddr.sin_family = AF_INET; /*internet address family*/ 
+//= domaine d'écoute carte réseau j'écoute ? //hton1 = conversion little endian big endian h(host) to nl ( network long) 
+	echoServAddr.sin_port = htons(atoi(argv[2])); /*Port local*/ 
+	 //htons = conversion little endian big endian h(host) to ns ( network short)
+	hid = gethostbyname(argv[1]);
+
 	
-	  initialise le socket TCP
-      fait une demande de connexion au serveur
-  	  boucle :
-  	       envoi de "n" objets "obj" (obtenus de iniobj.h)
-  	       le dernier objet envoyé contient un marqueur de fin
-  	       obj.iqt = -1
-      fermer la connexion
-      fin
-      
-      
-    */
-	
-	
+
+	bcopy(hid->h_addr, (char *)&echoServAddr.sin_addr,hid->h_length);
+
+
+
+//CONNECTION A LA SOCKET SERVEUR
+connect(ClntSocket,(struct sockaddr *) &echoServAddr,sizeof(echoServAddr));
+
+/*
+initialise le socket TCP
+fait une demande de connexion au serveur
+boucle :
+envoi de "n" objets "obj" (obtenus de iniobj.h)
+le dernier objet envoyé contient un marqueur de fin
+obj.iqt = -1
+fermer la connexion
+fin
+*/
+
+
+// ./clio localhost numero de port
+//argv 2 - Port du serveur
+
+
 
 
 return 0;
--- Sources/TD02/A/sero.c
+++ Sources/TD02/A/sero.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
 
 
 #include "defobj.h"
@@ -8,87 +10,160 @@
 
 #include <sys/socket.h>
 
-const int NB_MAX_CLIENT_SIMULTANES = 5;
+
+
+void traiterClient(int ClntSocket){
+
+
+	int iqtFound=0;
+	obj objBuffer;
+	int receivedDataSize;
+	if(receivedDataSize=recv(ClntSocket,objBuffer,sizeof(obj),0) < 0)
+	{
+		perror("Server - Erreur reception de message");
+		exit(-1);
+	} else 
+	{
+		while(receivedDataSize > 0){
+
+			//Afficher message		
+			//Regarder si -1;
+			//refaire un received comme dans le poly ?
+			
+		}
+	}
+
+
+	if()
+}
+/*boucle : lire data client sur socket
+		jusqu'à objet contenant "fin"*/
+
+}
 
 
 
-int main(void){
+int main(int argc, char* argv[]){
 
+	pid_t pid;
 	int ServSocket;
-	struct sockaddr_in echoServAddr; /*Adresse locale*/
+	int ClntSocket;
+	struct sockaddr_in echoServAddr; /*Adresse locale*/ //NB in = internet pas in/out
 	struct sockaddr_in echoClntAddr; /*Adresse client*/
 	int clntLen; /*Taille de la strucutre de donnees adresse client*/
+	int portNumber;
 
 
-	int ServSocket = socket(PF_INET,SOCK_STREAM,IPPROTO_TCP);
+	//Verification de la syntaxe d'appel
+	if(argc < 2)
+	{
+		fprintf(stderr,"Usage : %s <port number>\n",argv[0]);
+		exit(-1);
+	}
+
 	
+	//Récuperation du numero de port
+	portNumber=atoi(argv[1]);
+
+	//creation de la socket
+	ServSocket = socket(PF_INET,SOCK_STREAM,IPPROTO_TCP);
+	//PF_INET = domaine de la socket = internet
+	//SOCK_STREAM = Mode connecté = TCP
 	
-	
-	
+
+	//Traitement des erreur
 	if (ServSocket < 0){
-	
+
 		perror("Serveur : Erreur pendant la creation de la Socket");
 		exit(-1);
 	}
-	
-	
-	/*CONFIGURATION DE L'ADRESSE LOCALE*/
-	
+
+
+	/*Configuration de l'adresse locale*/
+	//utiliser bzero
 	memset(&echoServAddr,0,sizeof(echoServAddr)); /*mise à zero de la structure*/
-	
-	echoServAddr.sin_family = AF_INET; /*internet address family*/
-	echoServAddr.sin_addr.s_addr = hton1(INADDR_ANY); /*n'importe quelle interface entrante*/
-	echoServAddr.sin_port = htons(echoServAddr); /*Port local*/
-	
+
+	echoServAddr.sin_family = AF_INET; /*internet address family*/ //= domaine d'écoute
+	echoServAddr.sin_addr.s_addr = htonl(INADDR_ANY); /*n'importe quelle interface entrante*/ // = Sur quelle carte réseau j'écoute ? //hton1 = conversion little endian big endian h(host) to nl ( network long) 
+	echoServAddr.sin_port = htons(portNumber); /*Port local*/ 
+	 //htons = conversion little endian big endian h(host) to ns ( network short)
+
+	//LE RESEAU EST BIG ENDIAN
+	//INTEL EST LITTLE ENDIAN 
+
+	//ARM = Advanced RISC Machine -> Reduced Instruction-Set Computer > Eficient d'un pt de vue energétique
+
 	/*Bind a l'adresse locale*/
-	
-	if(bind(servSocket,(struct sockaddr_in *) &echoServAddr,sizeof(echoServAddr)) < 0)
+
+	if(bind(ServSocket,(struct sockaddr *) &echoServAddr,sizeof(echoServAddr)) < 0)
 	{
 		perror("Serveur : Erreur pendant le reliage de la Socket");
 		exit(-1);
 	}
-	
+
 	/*Mise en ecoute*/
-	
-	if(listen(servSock,NB_MAX_CLIENT_SIMULTANES) < 0){
-	
+
+	if(listen(ServSocket,SOMAXCONN) < 0){ //SOMAXCONN = Maximum des capacités de la socket
+
 		perror("Serveur : Erreur lors de la mise en ecoute");
 		exit(-1);
 	}
 
 	while(1){ //Boucle infinie
-	
-		/*Definir la taille du parametre d'entree-sortie*/
-		clntLen = sizeof(echoClntAddr);
-		
-		if(clntSock=accept(servSock,(struct sockaddr_in *) &echoClntAddr,&clntLen) < 0){
-		
-			perror("Serveur : Erreur pendant l'acceptation d'un client")
-			exit(-1);
-			
-		}
-		
-		
-		//ICI : TRAITEMENT CLIENT	
-	}
 
+	/*Definir la taille du parametre d'entree-sortie*/
+		clntLen = sizeof(echoClntAddr);
+
+		if(ClntSocket=accept(ServSocket,(struct sockaddr *) &echoClntAddr,&clntLen) < 0){ //accept est bloquant
+
+		perror("Serveur : Erreur pendant l'acceptation d'un client");
+		exit(-1);
+
+		}
+
+
+		//Traitement du client
+		pid=fork();
+
+		if(pid < 0) /*erreur*/
+		{
+			perror("Serveur - Erreur creation du processus fils");
+			exit(-1);
+
+		}else if(pid == 0) /*fils*/
+		{
+		
+		traiterClient();
+ 		/*
+		traiterclient()
+		
+
+		}else /*pere*/
+		{
+		/*
+		waitpid() attends fin du fils
+		si statut fin fils = arrêt : fin serveur au bout de trois clients*/
+		}
+	}	
 
 return 0;
 
 }
 /*
 
-      initialise le socket TCP
-      boucle :
-		  attente des connexions clients
-		  quand connexion client: fork()
-			  fils : traiterclient()
-				  boucle : lire data client sur socket
-						   jusqu'à objet contenant "fin"
-			  père : waitpid() attends fin du fils
-				  si statut fin fils = arrêt : fin serveur
+initialise le socket TCP
+boucle :
+attente des connexions clients
 
 
 */
 
 
+
+// boucler sur send tant que pas tt envoyer
+// boucler sur receive tant que pas tt envoyer
+// si erreur, le signifier et sortir
+//Pour eviter la confusion des serveurs avec les autres groupes de TD, on choisira un numéro de port 10 000 + n° compte SR03 + n° debug
+//quand debug plante, il faut fermer le serveur sinon erreur addresse déja utilisée, ajouter 100 au premier débug, 200 au deuxiem etc.. puis apres un certain temps, retourner a 0
+//le numéro de port sera passé en parametre
+// cat /etc/services <- liste des ports conventionée
